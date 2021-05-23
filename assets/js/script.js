e0bf29d244962ac21208b038491c66fb55b47527
Asa Grace <asag182@gmail.com>
2021-05-23 17:59:05 -0600

    add question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,56 @@
-var timeout = setTimeout(function(){""})
+var startButton = document.getElementById('start-btn')
 
-document.getElementById("Start Quiz").addEventListener("click", function() {
-    var timeleft = 30;
-    var downloadTimer = setInterval(function() {
-    if (timeleft <=0){
-        clearInterval(downloadTimer);
-    } 
-    document.getElementById("progressBar").value = 30 - timeleft;
-    timeleft -= 1;
-}, 1000);
-})
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+var questions = [
+    {
+        question: 'JavaScript arrays are used to store____?',
+        answers: [
+            { text: 'multiple values into a single variable', correct: true },
+            { text: 'Multiple values in a single function', correct: false },
+            { text: 'variables enclosed in {} ', correct: false },
+            { text: 'Store boolean inside the <div> ', correct: false }
+        ]
+    },
+    {
+        question: 'The condition of an if/and statement is enclosed with____.',
+        answers: [
+            { text: ' "" ', correct: false },
+            { text: ' {} ', correct: true },
+            { text: ' <> ', correct: false },
+            { text: ' () ', correct: false }
+        ]
+    },
+    {
+        question: 'Generate a random number using what syntax?',
+        answers: [
+            { text: 'Math.floor(Math.random())', correct: true },
+            { text: 'Math.random(Math.floor())', correct: false },
+            { text: '(Math.floor)(Math.random())', correct: false },
+            { text: '(Math.random)(Math.floor())', correct: false }
+        ]
+    },
+    {
+        question: 'What would you use to call a function',
+        answers: [
+            { text: 'call', correct: false },
+            { text: 'return', correct: false },
+            { text: 'summon', correct: false },
+            { text: 'bring forth please', correct: false }
+        ]
+    },
+]
